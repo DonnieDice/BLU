29d81f35a238df38f867f13b8cd80477c9ddba1d
joseph gettings <donniedice@protonmail.com>
2025-10-06 22:29:53 -0500

    feat: Migrate all event handlers to core module

--- modules/core.lua
+++ modules/core.lua
@@ -9,6 +9,8 @@ function core:OnEnable()
     self.addon:RegisterEvent("ACHIEVEMENT_EARNED", "HandleAchievementEarned")
     self.addon:RegisterEvent("HONOR_LEVEL_UPDATE", "HandleHonorLevelUpdate")
     self.addon:RegisterEvent("MAJOR_FACTION_RENOWN_LEVEL_CHANGED", "HandleRenownLevelChanged")
+    self.addon:RegisterEvent("PERKS_ACTIVITY_COMPLETED", "HandlePerksActivityCompleted")
+    self:ReputationChatFrameHook()
 end
 
 function core:HandlePlayerLevelUp()
@@ -35,6 +37,10 @@ function core:HandleRenownLevelChanged()
     self.addon:HandleEvent("MAJOR_FACTION_RENOWN_LEVEL_CHANGED", "RenownSoundSelect", "RenownVolume", defaultSounds[6], "MAJOR_FACTION_RENOWN_LEVEL_CHANGED_TRIGGERED")
 end
 
+function core:HandlePerksActivityCompleted()
+    self.addon:HandleEvent("PERKS_ACTIVITY_COMPLETED", "PostSoundSelect", "PostVolume", defaultSounds[9], "PERKS_ACTIVITY_COMPLETED_TRIGGERED")
+end
+
 function core:HandleEvent(eventName, soundSelectKey, volumeKey, defaultSound, debugMessage)
     if self.addon.functionsHalted then
         self.addon:PrintDebugMessage("FUNCTIONS_HALTED")
@@ -53,6 +59,94 @@ function core:HandleEvent(eventName, soundSelectKey, volumeKey, defaultSound, de
         self.addon.isProcessingQueue = true
         self.addon:ProcessEventQueue()
     end
+end
+
+function core:ReputationChatFrameHook()
+    if self.addon.chatFrameHooked then return end
+
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(_, _, msg)
+        self.addon:PrintDebugMessage("INCOMING_CHAT_MESSAGE: " .. msg)
+
+        local rankFound = false
+        if string.match(msg, "You are now Exalted with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Exalted|r")
+            self:ReputationRankIncrease("Exalted", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Revered with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Revered|r")
+            self:ReputationRankIncrease("Revered", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Honored with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Honored|r")
+            self:ReputationRankIncrease("Honored", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Friendly with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Friendly|r")
+            self:ReputationRankIncrease("Friendly", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Neutral with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Neutral|r")
+            self:ReputationRankIncrease("Neutral", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Unfriendly with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Unfriendly|r")
+            self:ReputationRankIncrease("Unfriendly", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Hostile with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Hostile|r")
+            self:ReputationRankIncrease("Hostile", msg)
+            rankFound = true
+        elseif string.match(msg, "You are now Hated with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Hated|r")
+            self:ReputationRankIncrease("Hated", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then -- start of new shit
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Acquaintance|r")
+            self:ReputationRankIncrease("Acquaintance", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Crony|r")
+            self:ReputationRankIncrease("Crony", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Accomplice|r")
+            self:ReputationRankIncrease("Accomplice", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Collaborator|r")
+            self:ReputationRankIncrease("Collaborator", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Accessory|r")
+            self:ReputationRankIncrease("Accessory", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Abettor|r")
+            self:ReputationRankIncrease("Abettor", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Conspirator|r")
+            self:ReputationRankIncrease("Conspirator", msg)
+            rankFound = true
+        elseif string.match(msg, "Your standing with") then
+            self.addon:PrintDebugMessage("|cff00ff00Rank found: Mastermind|r")
+            self:ReputationRankIncrease("Hated", msg)
+            rankFound = true
+        end
+
+        if not rankFound then
+            self.addon:PrintDebugMessage("NO_RANK_FOUND")
+        end
+
+        return false
+    end)
+
+    self.addon.chatFrameHooked = true
+end
+
+function core:ReputationRankIncrease(rank, msg)
+    local factionName = string.match(msg, "with (.+)")
+    self.addon:HandleEvent("REPUTATION_RANK_INCREASE", "RepSoundSelect", "RepVolume", defaultSounds[6], "REPUTATION_RANK_INCREASE_TRIGGERED")
 end
 
 BLULib = BLULib or {}
